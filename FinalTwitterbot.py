b646bc337fa26be1b12070efac6ca4a28e7f7926
tgomez <34220115+tgomez@users.noreply.github.com>
2018-03-24 11:43:10 -0700

    Update FinalTwitterbot.py

--- FinalTwitterbot.py
+++ FinalTwitterbot.py
@@ -1,14 +1,16 @@
-import pandas as pd
+
 import tweepy
 import time
 import json
 import random
 
+
+
 # Twitter API Keys
-from APITweeter2 import consumer_key
-from APITweeter2 import consumer_secret
-from APITweeter2 import access_token
-from APITweeter2 import access_token_secret
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
