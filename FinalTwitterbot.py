62fe62b2beb966870115e6611d109db81e1b14d7
Tiffany Gomez <34220115+tgomez@users.noreply.github.com>
2018-07-25 13:43:55 -0700

    Update FinalTwitterbot.py

--- FinalTwitterbot.py
+++ FinalTwitterbot.py
@@ -17,19 +17,19 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
     
 # Quotes to Tweet
-happy_quotes = [
-    "For every minute you are angry you lose sixty seconds of happiness. - Ralph Waldo Emerson",
-    "Folks are usually about as happy as they make their minds up to be. - Abraham Lincoln",
-    "Happiness is when what you think, what you say, and what you do are in harmony. - Mahatma Gandhi",
-    "Count your age by friends, not years. Count your life by smiles, not tears. - John Lennon",
-    "Happiness is a warm puppy. - Charles M. Schulz",
-    "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
-    "Now and then it's good to pause in our pursuit of happiness and just be happy. - Guillaume Apollinaire"]
+ToPost = [
+    "This is one status",
+    "Hola",
+    "Ca Va",
+    "Bonjour",
+    "Hullo",
+    "hi",
+    "Hello"]
 
 
 def tweetout(tweet_number):
-    happy_quote = random.choice(happy_quotes)
-    api.update_status(happy_quote)
+    ToPost = random.choice(ToPost)
+    api.update_status(ToPost)
     
 counter=0
 while(True):
